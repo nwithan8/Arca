c8cbd57537b980d00da72a8e56d9b08fb1386bff
Nate Harris <n8gr8gbln@gmail.com>
2019-08-07 19:27:46 -0400

    Added ESPN top function

--- espn/espn.py
+++ espn/espn.py
@@ -24,9 +24,6 @@ class Team(object):
         self.initials = initials
 
 class ESPN(commands.Cog):
-    
-    def __init__(self, bot):
-        self.bot = bot
         
     def fix_league(self, league):
         league = league.lower()
@@ -54,7 +51,8 @@ class ESPN(commands.Cog):
         
         #Credit to Josh Fuerst (http://www.fuerstjh.com) for creating his makeshift ESPN API after the official one was locked.
         scores = {}
-        STRIP = "()1234567890 "
+        STRIP = "1234567890 "
+        STRIP_BRACES = "()1234567890 "
         #visit espn bottomline website to get scores as html page
         url = 'http://www.espn.com/'+league+'/bottomline/scores'
         req = urllib.request.Request(url)
@@ -83,8 +81,10 @@ class ESPN(commands.Cog):
                                 
             #split score string into each teams string
             team1_name = ''
+            team1_rank = ''
             team1_score = '0'
             team2_name = ''
+            team2_rank = ''
             team2_score = '0'
                                 
             if (' at ' not in score):
@@ -93,18 +93,32 @@ class ESPN(commands.Cog):
                 team2_name = teams[1][0:teams[1].rfind(' ')].lstrip(STRIP)
                 team1_score = teams[0][teams[0].rfind(' ')+1:].strip()
                 team2_score = teams[1][teams[1].rfind(' ')+1:].strip()
+                if ') ' in team1_name:
+                    team1_rank = re.search('\((\d*)\)',team1_name).group(1)
+                    team1_name = team1_name.lstrip(STRIP_BRACES)
+                if ') ' in team2_name:
+                    team2_rank= re.search('\((\d*)\)',team2_name).group(1)
+                    team2_name = team2_name.lstrip(STRIP_BRACES)
             else:
                 teams = score.split(' at ')
                 team1_name = teams[0].lstrip(STRIP)
                 team2_name = teams[1].lstrip(STRIP)
+                if ') ' in team1_name:
+                    team1_rank = re.search('\((\d*)\)',team1_name).group(1)
+                    team1_name = team1_name.lstrip(STRIP_BRACES)
+                if ') ' in team2_name:
+                    team2_rank= re.search('\((\d*)\)',team2_name).group(1)
+                    team2_name = team2_name.lstrip(STRIP_BRACES)
                                         
             #add to return dictionary
-            scores[gameID] = ['','','','','']
+            scores[gameID] = ['','','','','','','']
             scores[gameID][0] = team1_name
-            scores[gameID][1] = team1_score
-            scores[gameID][2] = team2_name
-            scores[gameID][3] = team2_score
-            scores[gameID][4] = time
+            scores[gameID][1] = team1_rank
+            scores[gameID][2] = team1_score
+            scores[gameID][3] = team2_name
+            scores[gameID][4] = team2_rank
+            scores[gameID][5] = team2_score
+            scores[gameID][6] = time
         return scores
     
     @tasks.loop(count=1)
@@ -131,12 +145,8 @@ class ESPN(commands.Cog):
                 temp_dict[str(re.search('/id/(.*)/', str(sec.a.get('href'))).group(1))] = sec.find("img", {"class": "aspect-ratio--child"}).get('title')
             team_codes[league] = temp_dict
             
-        print(team_codes['mlb']['atl'])
-        
-    @commands.Cog.listener()
-    async def on_ready(self):
-        self.teams.start()
-    
+        print("Teams updated.")
+        print("ESPN ready.")
 
     @commands.group(aliases=["ESPN"])
     async def espn(self, ctx: commands.Context):
@@ -144,7 +154,7 @@ class ESPN(commands.Cog):
         Get live info from ESPN.com
         """
         if ctx.invoked_subcommand is None:
-            pass
+            await ctx.send("What subcommand?")
         
     @espn.command(name="prob")
     async def espn_prob(self, ctx: commands.Context, league: str, *, team: str):
@@ -160,10 +170,10 @@ class ESPN(commands.Cog):
             else:
                 searched_id = ""
                 for g in scores:
-                        if (scores[g][0].lower().strip() == team.lower().strip()) or (scores[g][2].lower().strip() == team.lower().strip()):
+                        if (scores[g][0].lower().strip() == team.lower().strip()) or (scores[g][3].lower().strip() == team.lower().strip()):
                             searched_id = g
                             break
-                embed = discord.Embed(title=scores[searched_id][0] + " " + scores[searched_id][1] + " - " + scores[searched_id][2] + " " + scores[searched_id][3] + " " + scores[searched_id][4])
+                embed = discord.Embed(title=("(" + scores[searched_id][1] + ") " if scores[searched_id][1] != '' else '') + scores[searched_id][0] + " " + scores[searched_id][2] + " - " + ("(" + scores[searched_id][4] + ") " if scores[searched_id][4] != '' else '') + scores[searched_id][3] + " " + scores[searched_id][5] + " " + scores[searched_id][6])
                 embed.set_thumbnail(url="https://image.flaticon.com/icons/png/128/870/870901.png")
                 try:
                     soup = BeautifulSoup(requests.get("http://www.espn.com/"+league+"/game?gameId=" + str(searched_id)).content)
@@ -183,6 +193,57 @@ class ESPN(commands.Cog):
         except Exception as e:
             print(str(e))
             await ctx.send("Something went wrong. Please try again.")
+            
+    @espn_prob.error
+    async def espn_prob_error(self, ctx, error):
+        await ctx.send("Please include <league> <team>")
+        
+    @espn.command(name="top")
+    async def espn_top(self, ctx: commands.Context, league: str):
+        """
+        Top ranked teams of a specific league
+        Supported leagues: CFB
+        """
+        league=self.fix_league(league)
+        if league in ['ncf']:
+            try:
+                scores=self.get_scores(league)
+                if not scores:
+                    await ctx.send("Oops there's no " + league.upper() + " games!")
+                else:
+                    embed = discord.Embed(title="Top Teams")
+                    embed.set_thumbnail(url="https://image.flaticon.com/icons/png/128/870/870901.png")
+                    respond = False
+                    games = ""
+                    page_count = 1;
+                    ranking = 1
+                    fc = 0
+                    top_games = {
+                        }
+                    for g in scores:
+                        if scores[g][1] != '' or scores[g][4] != '':
+                            if scores[g][1] != '' and scores[g][4] != '':
+                                top_games[min(scores[g][1],scores[g][4])] = ['','']
+                                top_games[min(scores[g][1],scores[g][4])][0] = str(g)
+                                top_games[min(scores[g][1],scores[g][4])][1] = scores[g]
+                            else:
+                                top_games[max(scores[g][1],scores[g][4])] = ['','']
+                                top_games[max(scores[g][1],scores[g][4])][0] = str(g)
+                                top_games[max(scores[g][1],scores[g][4])][1] = scores[g]
+                    for i in range(1,26):
+                        if str(i) in top_games:
+                            embed.add_field(name=("(" + top_games[str(i)][1][1] + ") " if top_games[str(i)][1][1] != '' else '') + ("**"+top_games[str(i)][1][0]+"** " if int(top_games[str(i)][1][2]) > int(top_games[str(i)][1][5]) else top_games[str(i)][1][0]+" ")+top_games[str(i)][1][2]+" - "+ ("(" + top_games[str(i)][1][4] + ") " if top_games[str(i)][1][4] != '' else '') + ("**"+top_games[str(i)][1][3]+"** " if int(top_games[str(i)][1][5]) > int(top_games[str(i)][1][2]) else top_games[str(i)][1][3]+" ")+top_games[str(i)][1][5],value=top_games[str(i)][1][6]+" - [Live](http://www.espn.com/"+league+"/game?gameId="+top_games[str(i)][0]+")",inline=False)
+                            #print(top_games[str(i)])
+                    await ctx.send(embed=embed)
+            except Exception as e:
+                print(str(e))
+                await ctx.send("Something went wrong. Please try again.")
+        else:
+            await ctx.send("Please try another league.")
+            
+    @espn_top.error
+    async def espn_top_error(self, ctx, error):
+        await ctx.send("Please include <league>")
         
     @espn.command(name="score")
     async def espn_score(self, ctx: commands.Context, league: str, *, team: str):
@@ -201,17 +262,35 @@ class ESPN(commands.Cog):
                 embed.set_thumbnail(url="https://image.flaticon.com/icons/png/128/870/870901.png")
                 respond = False
                 games = ""
+                page_count = 1
                 if team == 'all':
+                    fc = 0
                     for g in scores:
-                        embed.add_field(name=("**"+scores[g][0]+"** " if int(scores[g][1]) > int(scores[g][3]) else scores[g][0]+" ")+scores[g][1]+" - "+("**"+scores[g][2]+"** " if int(scores[g][3]) > int(scores[g][1]) else scores[g][2]+" ")+scores[g][3],value=scores[g][4],inline=False)
+                        embed.add_field(name=("(" + scores[g][1] + ") " if scores[g][1] != '' else '') + ("**"+scores[g][0]+"** " if int(scores[g][2]) > int(scores[g][5]) else scores[g][0]+" ")+scores[g][2]+" - "+ ("(" + scores[g][4] + ") " if scores[g][4] != '' else '') + ("**"+scores[g][3]+"** " if int(scores[g][5]) > int(scores[g][2]) else scores[g][3]+" ")+scores[g][5],value=scores[g][6]+" - [Live](http://www.espn.com/"+league+"/game?gameId="+g+")",inline=False)
+                        fc = fc + 1
+                        if fc >= 23:
+                            embed.add_field(name='\u200b',value="http://www.espn.com/"+league+"/scoreboard",inline=False)
+                            embed.title = "ESPN Scoreboard (Page " + str(page_count) + ")"
+                            page_count = page_count + 1
+                            await ctx.send(embed=embed)
+                            embed = discord.Embed(title="ESPN Scoreboard (Page " + str(page_count) + ")")
+                            embed.set_thumbnail(url="https://image.flaticon.com/icons/png/128/870/870901.png")
+                            fc = 0
+                        if len(embed) > 5900:
+                            embed.add_field(name='\u200b',value="http://www.espn.com/"+league+"/scoreboard",inline=False)
+                            embed.title = "ESPN Scoreboard (Page " + str(page_count) + ")"
+                            page_count = page_count + 1
+                            await ctx.send(embed=embed)
+                            embed = discord.Embed(title="ESPN Scoreboard (Page " + str(page_count) + ")")
+                            embed.set_thumbnail(url="https://image.flaticon.com/icons/png/128/870/870901.png")
                     embed.add_field(name='\u200b',value="http://www.espn.com/"+league+"/scoreboard",inline=False)
                     respond = True
                 else:
                     for g in scores:
-                        if (scores[g][0].lower().strip() == team.lower().strip()) or (scores[g][2].lower().strip() == team.lower().strip()):
-                            embed.add_field(name=("**"+scores[g][0]+"** " if int(scores[g][1]) > int(scores[g][3]) else scores[g][0]+" ")+scores[g][1]+" - "+("**"+scores[g][2]+"** " if int(scores[g][3]) > int(scores[g][1]) else scores[g][2]+" ")+scores[g][3],value=scores[g][4],inline=False)
-                            if not scores[g][4][1].isdigit():
-                                embed.add_field(name='\u200b',value="http://www.espn.com/"+league+"/boxscore?gameId="+g,inline=False)
+                        if (scores[g][0].lower().strip() == team.lower().strip()) or (scores[g][3].lower().strip() == team.lower().strip()):
+                            embed.add_field(name=("(" + scores[g][1] + ") " if scores[g][1] != '' else '') + ("**"+scores[g][0]+"** " if int(scores[g][2]) > int(scores[g][5]) else scores[g][0]+" ")+scores[g][2]+" - "+ ("(" + scores[g][4] + ") " if scores[g][4] != '' else '') + ("**"+scores[g][3]+"** " if int(scores[g][5]) > int(scores[g][2]) else scores[g][3]+" ")+scores[g][5],value=scores[g][6]+" - [Live](http://www.espn.com/"+league+"/game?gameId="+g+")",inline=False)
+                            #if not scores[g][6][1].isdigit():
+                            #embed.add_field(name='\u200b',value="http://www.espn.com/"+league+"/boxscore?gameId="+g,inline=False)
                             respond = True
                             break
                 if not respond:
@@ -221,6 +300,10 @@ class ESPN(commands.Cog):
         except Exception as e:
             print(str(e))
             await ctx.send("Something went wrong. Please try again.")
+            
+    @espn_score.error
+    async def espn_score_error(self, ctx, error):
+        await ctx.send("Please include <league> <team>")
             
     @espn.command(name="sched",aliases=["schedule"])
     async def espn_sched(self, ctx: commands.Context, league: str, *, team: str):
@@ -251,3 +334,12 @@ class ESPN(commands.Cog):
             #            print(sec.get(
             #except:
             #    await ctx.send("Sorry, couldn't reach ESPN.com")
+            
+    @espn_sched.error
+    async def espn_sched_error(self, ctx, error):
+        await ctx.send("Please include <league> <team>")
+        
+    def __init__(self, bot):
+        self.bot = bot
+        print("ESPN - updating teams...")
+        self.teams.start()
