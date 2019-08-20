6588b16fcbb35db5291fbf4253c762bb33ebc79c
Nate Harris <n8gr8gbln@gmail.com>
2019-08-19 21:06:31 -0400

    Added starter bot

---
+++ bot.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import discord
+from discord.ext import commands
+import sys, traceback, os
+
+PREFIX="*"
+
+bot = commands.Bot(command_prefix=PREFIX)
+
+formatter = commands.HelpCommand(show_check_failure=False)
+
+exts = [
+"espn.__init__",
+"plex.plex",
+"core.manager",
+"plex_manager.__init__",
+"jellyfin_manager.__init__",
+"emby_manager.__init__",
+"news.__init__"]
+
+for ext in exts:
+    bot.load_extension(ext)
+
+@bot.event
+async def on_ready():
+    print(f'\n\nLogged in as : {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
+    await bot.change_presence(status=discord.Status.idle,activity=discord.Game(name=f'the waiting game | {PREFIX}'))
+    print(f'Successfully logged in and booted...!\n')
+
+bot.run(os.environ.get("DISCORD_BOT_TOKEN"))
