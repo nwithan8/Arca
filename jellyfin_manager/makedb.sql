55f719d43df122e33c825928ac43e82f1d3a6a48
Nate Harris <nwithan8@users.noreply.github.com>
2019-08-20 01:43:29 -0400

    Update makedb.sql

--- jellyfin_manager/makedb.sql
+++ jellyfin_manager/makedb.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE JellyfinDiscord;
 USE JellyfinDiscord;
 CREATE TABLE users (DiscordID BIGINT, JellyfinUsername VARCHAR(100), JellyfinID VARCHAR(100));
-#CREATE TABLE regular_users (DiscordID BIGINT, PlexUsername VARCHAR(100));
 CREATE USER DiscordBot@localhost IDENTIFIED BY 'DiscordBot';
 GRANT ALL PRIVILEGES ON JellyfinDiscord.* to 'DiscordBot'@'localhost';
