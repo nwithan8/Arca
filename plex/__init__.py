11778160cdd6049f9e21e8ad182f253059a155fd
Nate Harris <n8gr8gbln@gmail.com>
2020-01-15 10:17:32 -0500

    Cleaning files

--- plex/__init__.py
+++
@@ -1,4 +0,0 @@
-from .plex import Plex
-
-def setup(bot):
-	bot.add_cog(Plex(bot))
--- plex/makedb.sql
+++
@@ -1,6 +0,0 @@
-#CREATE DATABASE PlexDiscord;
-USE PlexDiscord;
-CREATE TABLE users (DiscordID BIGINT, PlexUsername VARCHAR(100), PlexEmail VARCHAR(100), ExpirationStamp INT);
-#CREATE TABLE regular_users (DiscordID BIGINT, PlexUsername VARCHAR(100));
-#CREATE USER plexbot@localhost IDENTIFIED BY 'plexbot';
-#GRANT ALL PRIVILEGES ON PlexDiscord.* to 'plexbot'@'localhost';
