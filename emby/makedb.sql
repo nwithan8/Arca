8c723263ae01772ee4a55f5577a36e5486042fcd
Nate Harris <n8gr8gbln@gmail.com>
2020-01-27 02:34:10 -0500

    file cleanup

--- emby/makedb.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE EmbyDiscord;
-USE EmbyDiscord;
-CREATE TABLE users (DiscordID VARCHAR(100), EmbyUsername VARCHAR(100), EmbyID VARCHAR(200), ExpirationStamp INT, Note VARCHAR(5));
-CREATE USER DiscordBot@localhost IDENTIFIED BY 'DiscordBot';
-GRANT ALL PRIVILEGES ON EmbyDiscord.* to 'DiscordBot'@'localhost';
--- jellyfin/makedb.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE JellyfinDiscord;
-USE JellyfinDiscord;
-CREATE TABLE users (DiscordID VARCHAR(100), JellyfinUsername VARCHAR(100), JellyfinID VARCHAR(200), ExpirationStamp INT, Note VARCHAR(5));
-CREATE USER DiscordBot@localhost IDENTIFIED BY 'DiscordBot';
-GRANT ALL PRIVILEGES ON JellyfinDiscord.* to 'DiscordBot'@'%' WITH GRANT OPTION;
